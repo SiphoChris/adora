96eb41713cccfac5ef899a3bed5e53215404dc53
Sipho Nkebe <siphonkebe@gmail.com>
2025-02-11 12:18:28 +0200

    nav created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
+import Nav from './components/Nav'
+
 function App() {
 
   return (
-    <div>
-      hello
-    </div>
+    <header className='pt-8'>
+      <Nav/>
+    </header>
   )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import { ChevronDown } from "lucide-react";
+
+function Nav() {
+  return (
+    <nav className="flex justify-between shadow-md rounded-full w-1/2 py-6 px-12 mx-auto">
+      <img src="#" alt="logo" />
+      <ul className="flex gap-8">
+        <li className="nav-link">
+          <a href="#">Product</a>
+          <ChevronDown />
+        </li>
+        <li className="nav-link">
+          <a href="#">Getting started</a>
+          <ChevronDown />
+        </li>
+        <li className="nav-link">
+          <a href="#">Company</a>
+          <ChevronDown />
+        </li>
+        <li>
+          <a href="#" className="font-medium">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
