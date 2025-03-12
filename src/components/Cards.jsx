a4a313a89d5d9d9eb03208b422cb4933092432a7
Sipho Nkebe <siphonkebe@gmail.com>
2025-03-12 10:43:14 +0200

    responsiveness

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,9 +3,9 @@ import Card from './Card';
 
 function Cards() {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:grid-rows-1 gap-12 md:gap-16 mt-16 w-[90%] mx-auto">
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:grid-rows-1 gap-4 md:gap-16 mt-16 w-[90%] mx-auto">
       {cardContent.map((card, index) => (
-        <li key={index} className="even:-translate-y-8">
+        <li key={index} className="md:even:-translate-y-8">
           <Card 
             imageUrl={card.imageUrl}
             title={card.title}
